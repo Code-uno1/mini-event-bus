4456f6110b8036866d3168ed81425ddc799798a9
KahuguMugo <emmanuelkahugu@gmail.com>
2026-02-09 12:01:09 +0300

    started and finished the project man

--- app.js
+++ app.js
@@ -1,2 +1,36 @@
-const shit = "Manu doesn't understand shit about this project but he soon will!!!";
-console.log(shit);
+function createEventBus() {
+    const listeners = new Map()
+
+    function on(event, handler) {
+        if (!listeners.has(event)) {
+            listeners.set(event, [])
+        }
+        listeners.get(event).push(handler)
+    }
+
+    function emit(event, data) {
+        const handlers = listeners.get(event)
+        if (!handlers) return
+
+        for (const handler of handlers) {
+            handler(data)
+        }
+    }
+
+    return {on, emit}
+}
+
+
+// Test it like a system, not a demo. But the funny part is i don't know what am doing!!!
+
+const bus = createEventBus()
+
+bus.on("login", user => {
+    console.log("log:", user)
+})
+
+bus.on("login", user => {
+    console.log("analytics:", user)
+})
+
+bus.emit("login", { id: 42 })
